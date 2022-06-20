7865e46e7728eb6656cb80ea29bee3f9b5bb50c6
Obi Ohagwu <obiohagwu8@gmail.com>
2022-06-20 13:38:07 -0400

    fixed package issues

--- model.py
+++ model.py
@@ -3,16 +3,17 @@ import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from torchtext.datasets import Multi30k
-from torchtext.data import Field, BucketIterator
+from torchtext.legacy.data import Field, TabularDataset, BucketIterator, Iterator
 
+import spacy
 # We use spacy to load some desired languages
 
-spacy_jp = spacy.load("jp") # load japanese
-spacy_en = spacy.load("en") # load english
-spacy_ge = spacy.load("ge") # load german
+spacy_jp = spacy.load("ja_core_news_sm") # load japanese
+spacy_en = spacy.load("en_core_web_sm") # load english
+spacy_ge = spacy.load("de_core_news_sm") # load german
 
-def tokenize_jp(sometext):
-    return [tok.sometext for tok in spacy_jp.tokenizer(sometext)]
+#def tokenize_jp(sometext):
+ #   return [tok.sometext for tok in spacy_jp.tokenizer(sometext)]
 
 def tokenize_en(sometext):
     return [tok.sometext for tok in spacy_en.tokenizer(sometext)]
