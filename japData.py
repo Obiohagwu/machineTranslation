f5ef5ac68d9a66819c2a7415afc60ffdde1aef7d
Obi Ohagwu <obiohagwu8@gmail.com>
2022-06-25 13:10:24 -0400

    having dependecy issues..build_vocab err

--- japData.py
+++ japData.py
@@ -4,10 +4,20 @@ import torch.nn as nn
 import torch.optim as optim
 import pandas as pd 
 import spacy
+from torchtext.legacy.data import Field, TabularDataset, BucketIterator, Iterator
+
 
 # Lets import the data set
 trainDf = pd.read_csv("dataset/train.csv")
 valDf = pd.read_csv("dataset/val.csv")
+
+# Some hyperparameters 
+
+EPOCHS = 10
+N = 6
+attnHeads = 8
+ModelD = 512
+BATCH_SIZE = 20
 
 # Our dataset is quite wild. Let's clean it up a bit
 
@@ -24,14 +34,36 @@ spacy_jp = spacy.load("ja_core_news_sm") # load japanese
 spacy_en = spacy.load("en_core_web_sm") # load english
 # Now we toenize the sets
 def tokenize_jp(sentences):
-    return [tokenize.text for tokenize in jap.tokenizer(sentences)]
+    return [tokenize.text for tokenize in spacy_jp.tokenizer(sentences)]
 
 def tokenize_en(sentences):
-    return [tokenize.text for tokenize in en.tokenizer(sentences)]
+    return [tokenize.text for tokenize in spacy_en.tokenizer(sentences)]
+
+
+# We have to implement the data fields so we can build a vocabulary based off token
+japaneseText = Field(tokenize=tokenize_jp)
+englishText = Field(tokenize=tokenize_en, init_token='<sos>', eos_token='<eos>')
+
+dataField = [('japanese', japaneseText), ('english', englishText)]
+
+cleanTrainDf = cleanData(trainDf)
+cleanvalDf = cleanData(valDf)
+japaneseText = build_vocab(cleanTrainDf, cleanValDf)
+englishText = build_vocab(cleanTrainDf, cleanValDf)
+
+trainingIterator = BucketIterator(
+    cleanTrainDf,
+    batch_size=BATCH_SIZE,
+    sort_key=lambda a: len(a.english),
+    shuffle=True
+)
 
 
 def test():
     print("No RUNTIME Errs!")
 if __name__ == "__main__":
+    #print(tokenize_jp(trainDf))
+    batch = next(iter(trainingIterator))
+    print(batch.english)
     test()
-    print(cleanData(trainDf))
+    
