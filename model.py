c968990e1bffcc1deeb628bce3a43111ead1ac7a
Obi Ohagwu <obiohagwu8@gmail.com>
2022-06-26 17:41:46 -0400

    implemented EncodeLayer & DecodeLayer. Will continue later

--- model.py
+++ model.py
@@ -54,6 +54,22 @@ class PositionalEncoder(nn.Module):
         return x
 
 
+# We have to define the attention function
+def attention(q,k,v, d_k, mask=None, dropout=None):
+    # This function can be found in attn paper
+    scores = torch.matmul(q, k.transpose(-2, -2)).math.sqrt(d_k)
+
+    if mask is not None:
+        mask = mask.unsqueeze(1)
+        scores = scores.masked_fill(mask==0,-1e9)
+    scores = F.softmax(scores, dim=-1)
+
+    if dropout is not None:
+        scores = dropout(scores)
+    output = torch.matmul(scores, v)
+    return output
+
+
 # Let's implement multihead attn
 class multiHeadAttention(nn.Module):
     def __init__(self, heads, model_dim, dropout = 0.1):
@@ -94,20 +110,101 @@ class multiHeadAttention(nn.Module):
         return output
 
 
-# We have to define the attention function
-def attention(q,k,v, d_k, mask=None, dropout=None):
-    # This function can be found in attn paper
-    scores = torch.matmul(q, k.transpose(-2, -2)).math.sqrt(d_k)
 
-    if mask is not None:
-        mask = mask.unsqueeze(1)
-        scores = scores.masked_fill(mask==0,-1e9)
-    scores = F.softmax(scores, dim=-1)
+# We implement an encoder and decoder layer class that will be used in encode and decode class
 
-    if dropout is not None:
-        scores = dropout(scores)
-    output = torch.matmul(scores, v)
-    return output
+# After implementing those, we will finally get to final transformer mechanism
+
+#implement norm class
+class Norm(nn.Module):
+    def __init__(self, model_dim, eps=1e-6):
+        super().__init__()
+
+        self.size=model_dim
+
+        # We implemet 2 learnable paramseters (alpha, bias) to calibrate normalisation
+        self.alpha = nn.Parameter(torch.ones(self.size))
+        self.bias = nn.Parameter(torch.ones(self.size))
+        self.eps = eps 
+
+    def forward(self, x):
+        # Refer to paper to see norm formula/definition
+        norm = self.alpha * (x - x.mean(dim=-1, keepdim=True)) \
+        / (x.std(dim=-1, keepdim=True) + self.eps) + self.bias
+        return norm
+
+# We implement an encoder layer with a feedforward layer and a a mutliheadattn layer
+class EncoderLayer(nn.Module):
+    # Refer to papers explicating the encoder layer for understanding
+    def __init__(self, model_dim, heads, dropout=0.1):
+        super().__init__()
+        #initializing two norm params we defines earlier 
+        self.norm1 = Norm(model_dim)
+        self.norm2 = Norm(model_dim)
+        self.attntn = multiHeadAttention(heads, model_dim)
+        self.feed_forward = FeedForward(model_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        x_int = self.norm1(x)
+        x+=self.dropout1(self.attntn(x_int, x_int, x_int, mask))
+        x_int = self.norm2(x)
+        x+=self.dropout2(self.feed_forward(x_int))
+        return x 
+
+
+# We implement an encoder layer with a feedforward layer and 2 mutliheadattn layer       
+
+class DecodeLayer(nn.Module):
+    def __init__(self, model_dim, heads, dropout=0.1):
+        super().__init__()
+        self.norm1 = Norm(model_dim)
+        self.norm2 = Norm(model_dim)
+        self.norm3 = Norm(model_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+        self.attntn1 = multiHeadAttention(heads, model_dim)
+        self.attntn2 = multiHeadAttention(heads, model_dim)
+        self.feed_forward = FeedForward(model_dim).to(device=DEVICE)
+
+        def forward(self, x, exp_out, source_mask, target_mask):
+            x_int = self.norm1(x)
+            x+=self.dropout1(self.attntn1(x_int, x_int, x_int, target_mask))
+            x_int = self.norm2(x)
+            x+=self.dropout2(self.attntn2(x_int, exp_out, exp_out, source_mask))
+            x_int = self.norm3(x)
+            x+=self.dropout3(self.feed_forward(x_int))
+            return x 
+
+# We can then build a convenient cloning function that can generate multiple layers:
+def get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+
+# We can finally implement the encoder and decoder modules
+
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, model_dim, N, heads):
+        super().__init__()
+        self.N = N
+        self.embed = Embed(vocab_size, model_dim)
+        self.pos_enc = PositionalEncoder(model_dim)
+        self.layers = get_clones(EncoderLayer(model_dim, heads), N)
+        self.norm=Norm(model_dim)
+
+    def forward(self, source, mask):
+        x = self.embed(source)
+        x = self.pos_enc(x)
+        for j in range(N):
+            x = self.layers[i](x, mask)
+        return self.norm(x)
+
+
+
+
+
 
 
 
